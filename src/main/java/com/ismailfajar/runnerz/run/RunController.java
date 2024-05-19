b4c70b9689aa868ef595ddca723b55bd082e5e56
ismailfajar81 <ismailfajar81@.com>
2024-05-19 21:47:22 +0700

    Add List API

---
+++ src/main/java/com/ismailfajar/runnerz/run/RunController.java
@@ -0,0 +1,25 @@
+package com.ismailfajar.runnerz.run;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+public class RunController {
+
+
+    private RunRepository runRepository;
+
+    public RunController(RunRepository runRepository) {
+        this.runRepository = runRepository;
+    }
+
+    @GetMapping("/api/runs")
+    List<Run> findAll() {
+        return runRepository.findAll();
+    }
+
+   }
---
+++ src/main/java/com/ismailfajar/runnerz/run/RunRepository.java
@@ -0,0 +1,38 @@
+package com.ismailfajar.runnerz.run;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class RunRepository {
+
+    private List<Run> runs = new ArrayList<>();
+
+    List<Run> findAll() {
+        return runs;
+    }
+
+
+
+    @PostConstruct
+    private void init() {
+        runs.add(new Run(1,
+                "Monday Morning Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plus(30, ChronoUnit.MINUTES),
+                3,
+                Location.INDOOR));
+
+        runs.add(new Run(2,
+                "Wednesday Evening Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plus(60, ChronoUnit.MINUTES),
+                5,
+                Location.OUTDOOR));
+    }
+}
