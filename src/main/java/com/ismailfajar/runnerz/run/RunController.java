3436d0521dfe796bc2e5f07ddb58f4b1afee719d
ismailfajar81 <ismailfajar81@.com>
2024-05-19 22:42:15 +0700

    Add update and delete API

--- src/main/java/com/ismailfajar/runnerz/run/RunController.java
+++ src/main/java/com/ismailfajar/runnerz/run/RunController.java
@@ -1,10 +1,7 @@
 package com.ismailfajar.runnerz.run;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -29,10 +26,29 @@ public class RunController {
 
     @GetMapping("/{id}")
     Run findById(@PathVariable Integer id) {
-        Optional <Run> run = runRepository.findById(id);
+        Optional<Run> run = runRepository.findById(id);
         if (run.isEmpty()) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
         return run.get();
     }
-   }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    void create(@RequestBody Run run) {
+        runRepository.create(run);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    void update(@RequestBody Run run, @PathVariable Integer id) {
+        runRepository.update(run, id);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable Integer id) {
+        runRepository.delete(id);
+    }
+
+}
